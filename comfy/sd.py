c6e15cb36601b8d4f1b131df423359d049d31b4a
comfyanonymous <comfyanonymous@protonmail.com>
2026-04-03 03:09:58 -0400

    Revert "Fix some fp8 scaled checkpoints no longer working. (#13239)"
    This reverts commit 4504ee718ac9d3fc244abcb0f3d2d5619daf0e72.

--- comfy/sd.py
+++ comfy/sd.py
@@ -1724,8 +1724,6 @@ def load_diffusion_model_state_dict(sd, model_options={}, metadata=None, disable
     temp_sd = comfy.utils.state_dict_prefix_replace(sd, {diffusion_model_prefix: ""}, filter_keys=True)
     if len(temp_sd) > 0:
         sd = temp_sd
-        if custom_operations is None:
-            sd, metadata = comfy.utils.convert_old_quants(sd, "", metadata=metadata)
 
     custom_operations = model_options.get("custom_operations", None)
     if custom_operations is None:
